772515f25cbe3ba947055251cf61319eee3bfec2
Adeel Ahmad <adeelahmad14@hotmail.com>
2017-03-18 19:26:33 +0500

    Update load-hips.py

--- load-hips.py
+++ load-hips.py
@@ -1,23 +1,38 @@
 #!/usr/bin/env python
 # @Author: Adeel Ahmad
 
-import numpy as np
+from PIL import Image
+from io import BytesIO
+
+import healpy as hp
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from jpegtran import JPEGImage
+import numpy as np
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from astropy.coordinates import get_sun
+from astropy.time import Time
 
 # Rule followed: Tile N in order K -> NorderK / DirD / NpixN{.ext}
-S = 9  # 512 X 512 tile size assumed
-survey = 'xmmpneb2'
-survey = 'gaia'
-HEALPix_cell = '44785'
-order = str(int(np.floor(np.log2(int(HEALPix_cell)) - S)))
-angular_resolution = None
-D = str(np.around(int(HEALPix_cell), decimals=-(len(HEALPix_cell) - 1)))
+survey = 'DSSColor'
+geocentric_coords = np.array([0.93085172744576927, 0.35906913416283726, 0.067708333333333329])
+order = 7
+theta, phi = hp.vec2ang(np.array(geocentric_coords))
+nside = hp.order2nside(order)
+npixel = hp.ang2pix(nside, theta, phi)[0]
+directory = np.around(int(npixel), decimals=-(len(str(npixel)) - 1))
 
-# base_url = 'http://axel.u-strasbg.fr/HiPSCatService/I/337/' + survey \
-#            + '/Norder' + order + '/Dir' + D + '/Npix' + HEALPix_cell + '.png'
-base_url = 'http://alasky.u-strasbg.fr/DSS/DSSColor/Norder7/Dir90000/Npix91422.jpg'
+# t = Time('2016-03-20 4:30:00')
+# print(get_sun(t))
+# Returns a GCRS frame
+
+# Coordinates for the Crab Nebula
+# c = SkyCoord(ra=5.5755472222222*u.degree, dec=22.014472222222*u.degree, frame='icrs')
+# c = c.galactic
+# print(c)
+# How to convert this to GCRS?
+
+base_url = 'http://alasky.u-strasbg.fr/DSS/' + survey \
+           + '/Norder' + str(order) + '/Dir' + str(directory) + '/Npix' + str(npixel) + '.jpg'
 
 print('Choose a library for retreiving HiPS data: \n1. URLlib\n2. Requests')
 choice = input('Enter your choice: ')
@@ -29,24 +44,10 @@ if (choice is '1'):
 elif (choice is '2'):
     import requests
 
-    data = str.encode(requests.get(base_url).text)
+    data = requests.get(base_url).content
 else:
     print('Invalid choice')
 
-jpeg_image = JPEGImage(data)
-jpeg_image_array = np.asarray(jpeg_image, dtype=np.float32)
-print(jpeg_image_array.shape)
-
-# plt.imshow(data)
-# plt.show()
-# parsed_data = data.decode('utf-8')
-# phot_g_n_obs = []
-# with open('../data.tsv') as tsvfile:
-#     reader = csv.DictReader(tsvfile, delimiter='\t')
-#     for row in reader:
-#         phot_g_n_obs.append(int(row['phot_g_n_obs']))
-#
-# # plotting
-# plt.title('phot_g_n_obs plot')
-# plt.plot(phot_g_n_obs)
-# plt.show()
+tile = np.array(Image.open(BytesIO(data)))
+plt.imshow(tile)
+plt.show()
