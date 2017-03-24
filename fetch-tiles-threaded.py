b9c82ab9fc8565157c91abf4189cd89b2a27bc92
Adeel Ahmad <adeelahmad14@hotmail.com>
2017-03-24 08:16:16 +0500

    Update application.md

--- fetch-tiles-threaded.py
+++ fetch-tiles-threaded.py
@@ -5,6 +5,8 @@ import threading
 import urllib.request
 import grequests
 import time
+import asyncio
+import aiohttp
 
 urls = [
     'http://alasky.u-strasbg.fr/DSS/DSSColor/Norder7/Dir90000/Npix91422.jpg',
@@ -121,6 +123,20 @@ urls = [
     'http://alasky.u-strasbg.fr/DSS/DSSColor/Norder7/Dir90000/Npix91430.jpg',
     'http://alasky.u-strasbg.fr/DSS/DSSColor/Norder7/Dir90000/Npix91431.jpg',
 ]
+
+@asyncio.coroutine
+def fetch_url_aiohttp(url,idx):
+    response = yield from aiohttp.request('get', url)
+    response.close()
+
+start = time.time()
+
+loop = asyncio.get_event_loop()
+tasks = [asyncio.async(fetch_url_aiohttp(z, i)) for i, z in enumerate(urls)]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
+
+print("Elapsed Time aiohttp (with asyncio): %s" % (time.time() - start))
 
 def fetch_url_urllib(url):
     urlHandler = urllib.request.urlopen(url)
