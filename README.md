7abe45821f1a31d46955a552a6ac690bd762126c
Adeel Ahmad <adeelahmad14@hotmail.com>
2017-03-30 23:20:13 +0500

    Add 'build-wcs.py'

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 GSoC 2017 project — HiPS to Py
 ----------
 
-This is a repositroy concerned with testing / implementing various prototypes concerned with OpenAstropy's HiPS to Py project. This project is a part of Google Summer of Code 2017, and the original contributions will be made to [https://github.com/tboch/hips](https://github.com/tboch/hips) via pull requests.
+This repositroy is concerned with testing / implementing various prototypes related with OpenAstropy's HiPS to Py project. This project is a part of Google Summer of Code 2017, and the original contributions will be made to [https://github.com/tboch/hips](https://github.com/tboch/hips) via pull requests.
 
 **Dependencies** for this package include:
 
---
+++ examples/build-wcs.py
@@ -0,0 +1,44 @@
+# Set the WCS information manually by setting properties of the WCS
+# object.
+
+from __future__ import division, print_function
+
+import numpy
+from astropy import wcs
+from astropy.io import fits
+
+# Create a new WCS object.  The number of axes must be set
+# from the start
+w = wcs.WCS(naxis=2)
+
+# Set up an "Airy's zenithal" projection
+# Vector properties may be set with Python lists, or Numpy arrays
+w.wcs.crpix = [-234.75, 8.3393]
+w.wcs.cdelt = numpy.array([-0.066667, 0.066667])
+w.wcs.crval = [0, -90]
+w.wcs.ctype = ["RA---AIR", "DEC--AIR"]
+w.wcs.set_pv([(2, 1, 45.0)])
+
+# Some pixel coordinates of interest.
+pixcrd = numpy.array([[0, 0], [24, 38], [45, 98]], numpy.float_)
+
+# Convert pixel coordinates to world coordinates
+world = w.wcs_pix2world(pixcrd, 1)
+print(world)
+
+# Convert the same coordinates back to pixel coordinates.
+pixcrd2 = w.wcs_world2pix(world, 1)
+print(pixcrd2)
+
+# These should be the same as the original pixel coordinates, modulo
+# some floating-point error.
+assert numpy.max(numpy.abs(pixcrd - pixcrd2)) < 1e-6
+
+# Now, write out the WCS object as a FITS header
+header = w.to_header()
+
+# header is an astropy.io.fits.Header object.  We can use it to create a new
+# PrimaryHDU and write it to a file.
+hdu = fits.PrimaryHDU(header=header)
+# Save to FITS file
+# hdu.writeto('test.fits')
